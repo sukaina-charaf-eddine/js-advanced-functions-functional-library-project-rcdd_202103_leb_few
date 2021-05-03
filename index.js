cb79c74b7c4f10153ebbe2ac346b339fdba8401b
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-05-03 09:05:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// const fi = (function() {
-//   return {
-//     libraryMethod: function() {
-//       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
-//     },
-//
-//     each: function() {
-//
-//     },
-//
-//     map: function() {
-//
-//     },
-//
-//     reduce: function() {
-//
-//     },
-//
-//     functions: function() {
-//
-//     },
-//
-//
-//   }
-// })()
+const fi = (function() {
+  return {
+    libraryMethod: function() {
+      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+    },
+
+    each: function() {
+
+    },
+
+    map: function() {
+
+    },
+
+    reduce: function() {
+
+    },
+
+    functions: function() {
+
+    },
+
+
+  }
+})()
 
 fi.libraryMethod()
 const fi = (function() {
